536bb010f09467e92540846fc27da9695d03c4da
Aj-anushka <anushka.mwdku23@sinhgad.edu>
2024-07-14 09:04:01 +0530

    Add files via upload

---
+++ landing_page/script.js
@@ -0,0 +1,11 @@
+function openNavbar() {
+    document.getElementById("sideNavigationBar")
+        .style.width = "50%";
+}
+function closeNavbar() {
+    document.getElementById("sideNavigationBar")
+        .style.width = "0%";
+}
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Yor are signed up successfully---!";
+  }
